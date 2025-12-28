4fdca6f9b70798aee349b016b3a243a75cdf21bb
CHUANG <3418732870@qq.com>
2025-12-28 23:56:17 +0800

    水仙花数的练习  2025,12,28

---
+++ 水仙花数的判断.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x = 0;
+	int a, b, c;
+
+	printf("输入一个三位数: \n");
+//	scanf("%d", &x);
+	for (x = 1; x < 1000; x++) {
+		a = x % 10;
+		b = x / 10 % 10;
+		c = x / 100;
+	//	printf("%d  ,%d,  %d \n", a, b, c);
+
+		if (a * a * a + b * b * b + c * c * c == x) {
+			printf("Yes!  %d\n",x);
+
+		}
+	}
+	return 0;
+}
