a363a8822db387ab93b5bb727d1fce7ca9039399
CHUANG <3418732870@qq.com>
2026-01-17 10:51:31 +0800

    结构体与共用体

--- 结构体与共用体的使用--16.c
+++ 结构体与共用体的使用--16.c
@@ -3,17 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 typedef   struct student {
-	//	int a =0 ;
+		int a  ;
 		double b ;
+		char c;
+		long m;
+		
 	}st;
+	typedef union un {
+		int x ;
+		double b;
+		
+	}union1 ;
 int main(void) {
 	
 	
 	st student2 ;
 	student2.b =4;
 	
-	
-	
+	union1  union_1 ;
+	union_1.b=3;
+	union_1.x =4;
+	//printf("%d \t", union_1.x);
+	printf("%d",sizeof(student2.m));
 	
 	
 	return 0;
